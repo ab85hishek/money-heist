19216b12ce6932190b5207ec8fa037327e491803
Abhishek Ranjan <ab85hishek@gmail.com>
2020-05-21 11:35:25 +0530

    money heist characters

---
+++ src/Components/robots.js
@@ -0,0 +1,63 @@
+export const robots = [
+    {
+        id :1,
+        name :'Silene Oliveira',
+        username :'Tokyo',
+        email :'tokyo@gmail.com'
+    },
+    {
+        id :2,
+        name :'Andrés de Fonollosa',
+        username :'Berlin',
+        email :'berlin@gmail.com'
+    },
+    {
+        id :3,
+        name :'Agustín Ramos',
+        username :'Moscow',
+        email :'moscow@gmail.com'
+    },
+    {
+        id :4,
+        name :'Ágata Jiménez',
+        username :'Nairobi',
+        email :'nairobi@gmail.com'
+    },
+    {
+        id :5,
+        name :'Aníbal Cortés',
+        username :'Rio',
+        email :'rio@gmail.com'
+    },
+    {
+        id :6,
+        name :'Ricardo',
+        username :'Denver',
+        email :'denver@gmail.com'
+    },
+    {
+        id :7,
+        name :' Mirko Dragic',
+        username :'Helsinki',
+        email :'helsinki@gmail.com'
+    },
+    {
+        id :8,
+        name :'Dimitri Mostovói',
+        username :'Oslo',
+        email :'oslo@gmail.com'
+    },
+    {
+        id :9,
+        name :'Sergio Marquina',
+        username :'Pofessor',
+        email :'professor@gmail.com'
+    },
+    {
+        id :10,
+        name :'Raquel Murillo',
+        username :'Lisbon',
+        email :'lisbon@gmail.com'
+    }
+
+];
