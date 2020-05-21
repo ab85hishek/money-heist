8ea0db372cbbdcacb2da7f4a11576edd1b804f20
Abhishek Ranjan <ab85hishek@gmail.com>
2020-05-21 23:18:02 +0530

    Updates

---
+++ precache-manifest.795e2f9d3b7c3cafab25a94c5c5a2369.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f600d1a7889f4ae04d1894ffcd1401d2",
+    "url": "/money-heist/index.html"
+  },
+  {
+    "revision": "e4e191a241f10ece81d3",
+    "url": "/money-heist/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "35b52166916bfe4f45db",
+    "url": "/money-heist/static/css/main.6dc9f62c.chunk.css"
+  },
+  {
+    "revision": "e4e191a241f10ece81d3",
+    "url": "/money-heist/static/js/2.a702bcdf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/money-heist/static/js/2.a702bcdf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "35b52166916bfe4f45db",
+    "url": "/money-heist/static/js/main.4a85b6f6.chunk.js"
+  },
+  {
+    "revision": "f631a5543b3183e4ac9c",
+    "url": "/money-heist/static/js/runtime-main.0154fdca.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/money-heist/static/media/SEGA.12a3db8b.woff"
+  }
+]);
